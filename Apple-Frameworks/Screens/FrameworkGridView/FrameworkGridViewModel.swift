ac6e868fe04773f618f2d4e694bfabc68cd7455e
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2025-02-17 15:13:50 -0300

    Add files via upload

---
+++ Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Sean Allen on 10/31/20.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    @Published var isShowingDetailView = false
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+}
